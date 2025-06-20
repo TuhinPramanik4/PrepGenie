9ba0fc6976a8bd1c4d1e4681d498e3c8fb9ce924
ISHA007jain <isha35794@gmail.com>
2025-06-21 05:02:19 +0530

    added civic authentication

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import InterviewPage from './Interview/page'
-import AIInterviewPage from './ai-interview/page'
-import Dashboard from './dashboard/page'
-import InterviewResultsPage from './Interview-results/page'
-import LandingPage from './page'
-function App() {
+import React from 'react';
+import { CivicAuthProvider } from '@civic/auth/react'; // Don't forget this import!
+import InterviewPage from './Interview/page';
+import AIInterviewPage from './ai-interview/page';
+import Dashboard from './dashboard/page';
+import InterviewResultsPage from './Interview-results/page';
+import LandingPage from './page';
+
+function App({ children }) {
   return (
-    <div>
-      <Dashboard />
-    </div>
-  )
+    <CivicAuthProvider clientId="4fd94ee1-c831-4252-beed-d0c4c29d1e88">
+      <>
+        {children}
+        <div>
+          <LandingPage />
+        </div>
+      </>
+    </CivicAuthProvider>
+  );
 }
 
-export default App
+export default App;
